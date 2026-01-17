432a3eb0f352f909aa93a385be1131fbd3b1a028
prem kumar <117892300+premkandregula@users.noreply.github.com>
2026-01-17 23:45:22 +0530

    initial commit

--- src/main/java/com/StudentManagementSystem/controller/Controller.java
+++ src/main/java/com/StudentManagementSystem/controller/Controller.java
@@ -1,13 +1,14 @@
 package com.StudentManagementSystem.controller;
 
-import com.StudentManagementSystem.entity.Student;
-import com.StudentManagementSystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.StudentManagementSystem.entity.Student;
+import com.StudentManagementSystem.service.StudentService;
 
 @org.springframework.stereotype.Controller
 
